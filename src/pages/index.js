82858b1ea1b6f254657cd0e0be1ed51efb5a0a2a
s106631@ap.be <kasper.bosteels@student.ap.be>
2022-01-07 22:59:43 +0100

    banner en featured scooters toegevoegd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,100 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Scooter from "../components/scooter"
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  headerTitle,
+  scooters,
+} from "../components/page.module.css"
+const IndexPage = ({
+  data: {
+    wpPage: { homePageFields },
+  },
+}) => {
+  const scooterimage = getImage(homePageFields.bannerImage.localFile)
+  const featuredscooters = homePageFields.featuredVespa
+  return (
+    <Layout>
+      <div className={header}>
+        <div className={headerInfo}>
+          <h1 className={headerTitle}>{homePageFields.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: homePageFields.shortDescription,
+            }}
+          />
+        </div>
+        <div>
+          <GatsbyImage
+            className={headerPicture}
+            image={scooterimage}
+            alt={homePageFields.bannerImage.altText}
+          />
+        </div>
+      </div>
+      <div>
+        <h2>{featuredscooters.title}</h2>
+        <p>{featuredscooters.description}</p>
+        <div className={scooters}>
+          {featuredscooters.scooter.map(scooter => {
+            return (
+              <Scooter
+                slug={`scooters/${scooter.slug}`}
+                scooter={scooter}
+                key={scooter.id}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
-
+export const query = graphql`
+  query {
+    wpPage(slug: { eq: "home-page" }) {
+      homePageFields {
+        title
+        shortDescription
+        bannerImage {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+        featuredVespa {
+          scooter {
+            ... on WpScooter {
+              id
+              scooterFields {
+                name
+                pictures {
+                  picture1 {
+                    altText
+                    localFile {
+                      childImageSharp {
+                        gatsbyImageData(placeholder: BLURRED)
+                      }
+                    }
+                  }
+                }
+                description
+              }
+              slug
+            }
+          }
+          description
+          title
+        }
+      }
+    }
+  }
+`
 export default IndexPage
