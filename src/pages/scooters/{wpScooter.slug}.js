b6b6229cc547d386fe4284749b48be2e11ca794e
s106631@ap.be <kasper.bosteels@student.ap.be>
2022-01-09 01:03:54 +0100

    data in een tabel gezet en array can kleuren

--- src/pages/scooters/{wpScooter.slug}.js
+++ src/pages/scooters/{wpScooter.slug}.js
@@ -2,27 +2,70 @@ import * as React from "react"
 import Layout from "../../components/layout"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-
+import {
+  colorOrb,
+  imagecontainer,
+  image,
+  specificationstable,
+  colorsTable,
+} from "../../components/scooterSpecification.module.css"
 const ScooterPage = ({
   data: {
     wpScooter: { scooterFields },
+    wpScooter: { colors },
   },
 }) => {
   const scooterimage = getImage(scooterFields.pictures.picture1.localFile)
   const altText = scooterFields.pictures.picture1.altText
   return (
     <Layout pageTitle={scooterFields.name}>
-      <div>
-        <GatsbyImage image={scooterimage} alt={altText} />
+      <div classname={imagecontainer}>
+        <GatsbyImage className={image} image={scooterimage} alt={altText} />
         <p>{scooterFields.description}</p>
       </div>
-      <div>
-        <h3>specificaties</h3>
-        <p>cylinder inhoud: {scooterFields.cc}</p>
-        <p>motor cyclussen: {scooterFields.enginestroke}</p>
-        <p>wiel breedte: {scooterFields.wheelswidth}</p>
-        <p>tank inhoud: {scooterFields.fuelcapacity}</p>
-        <p>Km/L: {scooterFields.kmperliter}</p>
+      <div classname="tables">
+        <table classname={specificationstable}>
+          <tr>
+            <th>Specifications</th>
+            <th> </th>
+          </tr>
+          <tr>
+            <td>CC(cubic capacity)</td>
+            <td>{scooterFields.cc}</td>
+          </tr>
+          <tr>
+            <td>engine stroke</td>
+            <td>{scooterFields.enginestroke}</td>
+          </tr>
+          <tr>
+            <td>wheel width</td>
+            <td>{scooterFields.wheelswidth}</td>
+          </tr>
+          <tr>
+            <td>fuel capacity</td>
+            <td>{scooterFields.fuelcapacity}L</td>
+          </tr>
+          <tr>
+            <td>Km/L</td>
+            <td>{scooterFields.kmperliter}</td>
+          </tr>
+        </table>
+        <table className={colorsTable}>
+          <tr>
+            <th>Available colors:</th>
+            {colors.nodes.map(color => {
+              const scooterAvailableColor = color.name
+              return (
+                <td
+                  classname={colorOrb}
+                  style={{ color: `${scooterAvailableColor}` }}
+                >
+                  ⬤
+                </td>
+              )
+            })}
+          </tr>
+        </table>
       </div>
     </Layout>
   )
